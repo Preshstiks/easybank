022b916bcb82c970758b53dd110833edf4b3cd05
Davies Precious <preshy.d5@gmail.com>
2023-09-17 18:11:36 +0100

    changes made

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import { AiOutlineMenu, AiOutlinePlus } from "react-icons/ai";
+import { AnimatePresence, motion } from "framer-motion";
 import Logo from "../assets/img/logo.svg";
 import { useState } from "react";
 const Navbar = () => {
@@ -19,35 +20,42 @@ const Navbar = () => {
             <AiOutlineMenu className="font-bold hover:bg-[#00CC66] hover:text-white text-2xl" />
           )}
         </div>
-        {showMenu && (
-          <div className="text-[#333366] shadow-sm shadow-stone-300 md:text-left text-center font-normal absolute md:hidden left-[10%] top-[110px] bg-white w-[80%] py-6 rounded-xl">
-            <div className="py-3 px-4">
-              <span className="cursor-pointer hover:border-b-2 hover:border-[#00CC66] py-3">
-                Home
-              </span>
-            </div>
-            <div className="py-3 px-4">
-              <span className="cursor-pointer hover:border-b-2 hover:border-[#00CC66] pb-3">
-                About
-              </span>
-            </div>
-            <div className="py-3 px-4">
-              <span className="cursor-pointer hover:border-b-2 hover:border-[#00CC66] pb-3">
-                Contact
-              </span>
-            </div>
-            <div className="py-3 px-4">
-              <span className="cursor-pointer hover:border-b-2 hover:border-[#00CC66] pb-3">
-                Blog
-              </span>
-            </div>
-            <div className="py-3 px-4">
-              <span className="cursor-pointer hover:border-b-2 hover:border-[#00CC66] pb-3">
-                Careers
-              </span>
-            </div>
-          </div>
-        )}
+        <AnimatePresence>
+          {showMenu && (
+            <motion.div
+              initial={{ y: -100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              exit={{ y: -100, opacity: 0 }}
+              className="text-[#333366] shadow-sm shadow-stone-300 md:text-left text-center font-normal absolute md:hidden left-[10%] top-[110px] bg-white w-[80%] py-6 rounded-xl"
+            >
+              <div className="py-3 px-4">
+                <span className="cursor-pointer hover:border-b-2 hover:border-[#00CC66] py-3">
+                  Home
+                </span>
+              </div>
+              <div className="py-3 px-4">
+                <span className="cursor-pointer hover:border-b-2 hover:border-[#00CC66] pb-3">
+                  About
+                </span>
+              </div>
+              <div className="py-3 px-4">
+                <span className="cursor-pointer hover:border-b-2 hover:border-[#00CC66] pb-3">
+                  Contact
+                </span>
+              </div>
+              <div className="py-3 px-4">
+                <span className="cursor-pointer hover:border-b-2 hover:border-[#00CC66] pb-3">
+                  Blog
+                </span>
+              </div>
+              <div className="py-3 px-4">
+                <span className="cursor-pointer hover:border-b-2 hover:border-[#00CC66] pb-3">
+                  Careers
+                </span>
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
         <div className="text-[#999999] font-normal md:block hidden bg-white space-x-4">
           <span className="cursor-pointer hover:text-[#333366] hover:border-b-2 hover:border-[#00CC66] hover:pb-7">
             Home
